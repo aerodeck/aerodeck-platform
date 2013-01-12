f47e19f3ffb912b948fc94b2c72a9f064cd0c2e5
jawerty <jawerty210@gmail.com>
2013-01-12 18:51:18 -0500

    GET /users functionality

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,44 @@
 require( '../db' );
 var mongoose = require('mongoose');
 var user     = mongoose.model( 'user' );
+var bcrypt = require('bcrypt');
+var date = new Date();
 
 exports.newUser = function(req, res) {
+  
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  //date
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+  var day = date.getDate();
+  //Make it look nice (Official format TBD)
+  var time = month + '/' + day + '/' + year + ' at ' + hours + ':' + minutes + ':' + seconds;
+  
+  //requested bodies
+  full_name = req.body.user.full_name;
+  username = req.body.user.username;
+  email = req.body.user.email;
+  password = req.body.user.password;
+  apnToken = req.body.user.apn;
+  gcmToken = req.body.user.gcm;
+
+  //password hashing
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(password, salt);
+
+  newUser = user({
+    fullName: full_name,
+    username: username,
+    email: email,
+    password: hash,
+    apn: apnToken,
+    gcm: gcmToken,
+    created: date,
+    updated: null
+  }).save()
+  
   console.log('POST /users');
 };
 
