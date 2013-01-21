79969acd272d06c7168fa53bd51f9b24ebb30435
jawerty <jawerty210@gmail.com>
2013-01-20 19:37:44 -0500

    Added cross domain access

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ var express = require('express')
   , path = require('path');
 
 var app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    } else {
+      next();
+    }
+};
 
 app.configure(function() {
+  app.use(allowCrossDomain)
   // Basic setup
   app.set('port', 8000);
 
